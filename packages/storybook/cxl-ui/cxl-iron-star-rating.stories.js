c2bab3d89a127c86f22291cc3534c454afe020ee
Grzegorz Jasiński <grzegorz.jasinski@polcode.net>
2021-01-14 14:30:19 +0100

    add star rating demo

---
+++ packages/storybook/cxl-ui/cxl-iron-star-rating.stories.js
@@ -0,0 +1,26 @@
+import { html } from 'lit-html';
+import '@conversionxl/cxl-ui/src/components/cxl-institute-layout.js';
+import '@cwmr/iron-star-rating';
+import { withKnobs, boolean, number } from '@storybook/addon-knobs';
+
+export default {
+  title: 'CXL UI',
+  decorators: [withKnobs]
+};
+
+const Template = () => {
+  const isDisabled = boolean('Disabled', false);
+  const starsCount = number('Stars Count', 3);
+  return html`
+    <cxl-institute-layout id="container" theme="2c-l">
+      <h2>Iron Star Rating</h2>
+      <iron-star-rating ?readonly=${isDisabled} value=${starsCount}></iron-star-rating>
+    </cxl-institute-layout>
+  `;
+};
+
+export const CxlIronStarRating = Template.bind({});
+
+CxlIronStarRating.story = {
+  name: 'cxl-iron-star-rating'
+};
