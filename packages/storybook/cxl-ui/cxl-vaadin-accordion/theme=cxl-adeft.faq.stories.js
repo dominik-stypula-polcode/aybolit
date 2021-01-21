0046bd37620363bbe221cf7e231432c9231b649c
Dominik Stypuła <dominik.stypula@polcode.net>
2021-01-21 18:00:58 +0100

    adeft accordion styled

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/theme=cxl-adeft.faq.stories.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/theme=cxl-adeft.faq.stories.js
@@ -5,7 +5,7 @@ import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import faqData from './theme=cxl-faq.data.json';
 
 export default {
-  title: 'CXL UI|cxl-vaadin-accordion',
+  title: 'CXL UI|cxl-adeft-accordion',
   decorators: [withKnobs]
 };
 
@@ -21,29 +21,37 @@ const Template = () => {
         width: ${width}px;
         height: ${height}px;
       }
+      cxl-adeft-accordion {
+        padding: 10px;
+      }
     </style>
     <h3>Frequently Asked Questions</h3>
 
-    <cxl-adeft-accordion
-      id="cxl-vaadin-accordion-26107"
-      class="archive archive-faq plural"
-      theme="cxl-faq"
-    >
+    <cxl-adeft-accordion id="cxl-vaadin-accordion-26107" theme="reverse">
       ${faqData.map(
         el => html`
           <vaadin-accordion-panel
             id="${el.cxl_hybrid_attr_post['@attributes'].id}"
             class="${el.cxl_hybrid_attr_post['@attributes'].class}"
-            theme="cxl-faq"
+            theme="reverse"
           >
-            <header class="entry-header" slot="summary">
-              <h5 class="entry-title" itemprop="headline">
-                <a>${unsafeHTML(el.title.rendered)}</a>
-              </h5>
-            </header>
-            <div class="entry-summary" itemprop="description">
-              <span>${unsafeHTML(el.content.rendered)}</span>
+            <div class="accordionSummary" slot="summary">
+              <div class="left">
+                <vaadin-checkbox value="Option" theme="custom"></vaadin-checkbox>
+              </div>
+              <div class="right">
+                <div class="summaryTop">
+                  ${unsafeHTML(el.title.rendered)}
+                </div>
+              </div>
             </div>
+            <vaadin-vertical-layout>
+              <div class="itemContent">
+                <div class="checked">
+                  ${unsafeHTML(el.content.rendered)}
+                </div>
+              </div>
+            </vaadin-vertical-layout>
           </vaadin-accordion-panel>
         `
       )}
