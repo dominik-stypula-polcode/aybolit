24576dd57c74b4a435e7e6980ae3706a044de867
Dominik Stypuła <dominik.stypula@polcode.net>
2021-01-27 18:23:41 +0100

    playbook viewer layout development: moving styles from adeft-blocks, installing dependencies, refactoring to --lumo vars etc.

---
+++ packages/storybook/cxl-playbook-viewer/DataAdapter.js
@@ -0,0 +1,262 @@
+class DataAdapter {
+  /**
+   * @param {Object} apiData
+   */
+  constructor(apiData) {
+    this.mockBio = '';
+    this.apiData = apiData.data ? apiData.data : apiData;
+  }
+
+  /**
+   *
+   * @param { Array | undefined } obj
+   * @return {number|number}
+   * @private
+   */
+  // eslint-disable-next-line class-methods-use-this
+  _getLength(obj) {
+    if (!(obj instanceof Array)) {
+      // eslint-disable-next-line no-console
+      console.log(obj);
+      throw new Error('object is not an array!');
+    }
+    return obj.length || 0;
+  }
+
+  /**
+   *
+   * @return {{userHasVoted: boolean, rating: number}}
+   */
+  getRatingData() {
+    return {
+      rating: this.getRating(),
+      userHasVoted: this.getHasUserVoted()
+    };
+  }
+
+  /**
+   *
+   * @return {number}
+   */
+  getRating() {
+    if (!this.apiData.userVote) {
+      return 0;
+    }
+    return parseFloat(this.apiData.userVote);
+  }
+
+  /**
+   *
+   * @return {boolean}
+   */
+  getHasUserVoted() {
+    return Boolean(this.apiData.hasUserVoted);
+  }
+
+  /**
+   *
+   * @return {number}
+   */
+  getPlaybookTimestamp() {
+    if (!this.apiData.datePosted || this.apiData.datePosted.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('apiData.datePosted (Playbook date) is empty from API');
+      return 0;
+    }
+    // eslint-disable-next-line radix
+    return parseInt(this.apiData.datePosted) * 1000; // seconds(PHP) to miliseconds(JavaScript)
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  getPlaybookTitle() {
+    if (!this.apiData.title || this.apiData.title.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('apiData.title (Playbook title) is empty from API');
+      return '';
+    }
+    return String(this.apiData.title);
+  }
+
+  /**
+   *
+   * @return {number}
+   */
+  getUserId() {
+    // eslint-disable-next-line radix
+    return parseInt(this.apiData.userId) || 0;
+  }
+
+  /**
+   *
+   * @return {{anchors: {active, href, content}[]}|{anchors: []}}
+   */
+  getBreadcrumbsData() {
+    const breadCrumbs = this.apiData.breadcrumbs || [];
+    const cnt = this._getLength(breadCrumbs);
+    if (cnt < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('apiData.breadcrumbs is empty from API');
+      return {
+        anchors: []
+      };
+    }
+    const anchors = breadCrumbs.map((val, index) => {
+      return {
+        href: val.taxonomyName || '#',
+        content: val.displayName || '[EMPTY_NAME]',
+        active: !(index + 1 < cnt)
+      };
+    });
+    return {
+      anchors
+    };
+  }
+
+  /**
+   *
+   * @return {{items: []}|{items: {itemContent: *, time: string, title: string}[]}}
+   */
+  getAccordionData() {
+    const steps = this.apiData.steps || [];
+    const cnt = this._getLength(steps);
+    if (cnt < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('apiData.steps (Playbook steps) is empty from API');
+      return {
+        items: []
+      };
+    }
+    // eslint-disable-next-line no-unused-vars
+    let items = steps.map((val, index) => {
+      return {
+        title: `${val.idx}. ${val.title}`,
+        time: '[time_placeholder]',
+        itemContent: val.content
+      };
+    });
+    if (items.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('items (Playbook steps) is empty');
+      items = [];
+    }
+    return {
+      items
+    };
+  }
+
+  /**
+   *
+   * @param dataObj
+   * @return {{profileUrl: (string), avatarUrl: string, bio: string, id: number, email: string, username: string}}
+   * @private
+   */
+  // eslint-disable-next-line class-methods-use-this
+  _getPersonAll(dataObj) {
+    const person = dataObj;
+    if (!dataObj) {
+      // eslint-disable-next-line no-console
+      console.warn('person object is empty', dataObj);
+      return null;
+    }
+    if (!person || person.displayName.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('displayName is empty from API', dataObj);
+      person.displayName = '';
+    }
+    if (!person.profilePicture || person.profilePicture.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('profilePicture is empty from API', dataObj);
+      person.profilePicture = '';
+    }
+    if (!person.bio || person.bio.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('bio is empty from API', dataObj);
+      person.bio = '';
+    }
+    return {
+      id: person.id || 0,
+      bio: person.bio || '',
+      email: person.email || '',
+      username: person.displayName || '',
+      avatarUrl: person.profilePicture || '',
+      profileUrl: person.profileUrl || '#'
+    };
+  }
+
+  /**
+   *
+   * @return {{profileUrl: (string), avatarUrl: string, bio: string, id: number, email: string, username: string, timestamp: (number|*), likes: number}}
+   */
+  getAuthor() {
+    const author = this._getPersonAll(this.apiData.author);
+    return {
+      ...author,
+      bio: author.bio || this.mockBio
+    };
+  }
+
+  /**
+   *
+   * @return null|{{profileUrl: (string), avatarUrl: string, bio: string, id: number, email: string, username: string}}
+   */
+  getEditor() {
+    return this._getPersonAll(this.apiData.editor);
+  }
+
+  /**
+   *
+   * @return null|{{profileUrl: (string), avatarUrl: string, bio: string, id: number, email: string, username: string}}
+   */
+  getExpert() {
+    const expert = this.apiData.expert[0] || null;
+    return this._getPersonAll(expert);
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  getEditorName() {
+    const person = this.getEditor();
+    if (person && person.username) {
+      return person.username;
+    }
+    // eslint-disable-next-line no-console
+    console.warn('apiData.editor.displayName is empty from API');
+    return '';
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  getExpertName() {
+    const person = this.getExpert();
+    if (person && person.username) {
+      return person.username;
+    }
+    // eslint-disable-next-line no-console
+    console.warn('apiData.expert[x].displayName is empty from API');
+    return '';
+  }
+
+  /**
+   *
+   * @return {{description: ({description}|*|string)}}
+   */
+  getUseCase() {
+    const str = this.apiData.useCase || '';
+    if (!str || str.length < 1) {
+      // eslint-disable-next-line no-console
+      console.warn('apiData.useCase is empty from API');
+    }
+    return {
+      description: str
+    };
+  }
+}
+
+export default DataAdapter;
--- packages/storybook/cxl-playbook-viewer/cxl-playbook-viewer.stories.js
+++ packages/storybook/cxl-playbook-viewer/cxl-playbook-viewer.stories.js
@@ -2,17 +2,33 @@ import { html } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import '@conversionxl/cxl-ui/src/components/cxl-institute-layout.js';
 import '@conversionxl/cxl-ui/src/components/cxl-marketing-nav.js';
+import '@cwmr/iron-star-rating';
+import '@polymer/paper-tooltip/paper-tooltip';
+import moment from 'moment';
+import playbookViewerCSS from '@conversionxl/cxl-ui/src/styles/playbook-viewer-css.js';
+import { registerGlobalStyles } from '@conversionxl/cxl-lumo-styles/src/utils';
 import jsonData from './cxl-playbook.data.json';
+import DataAdapter from './DataAdapter';
 
 export default {
   title: 'CXL Playbook Viewer'
 };
 
 export const CxlPlaybookViewerLayout = () => {
-  /**
-   * @TODO: import icons like this: https://github.com/conversionxl/aybolit/blob/master/packages/cxl-lumo-styles/src/icons.js
-   *
-   */
+  const dataAdapter = new DataAdapter(jsonData.data);
+  const authorObj = dataAdapter.getAuthor();
+  const userLoggedIn = false; // dataAdapter.getUserId() > 0;
+  const userHasVoted = false;
+
+  registerGlobalStyles(playbookViewerCSS, {
+    moduleId: 'cxl-playbook-viewer-layout-global'
+  });
+
+  if (userHasVoted) {
+    window.addEventListener('DOMContentLoaded', () => {
+      document.querySelector('iron-star-rating').setAttribute('readonly', 'readonly');
+    });
+  }
 
   return html`
     <cxl-institute-layout id="container" theme="2c-l">
@@ -42,21 +58,108 @@ export const CxlPlaybookViewerLayout = () => {
           </vaadin-tab> </vaadin-tabs
       ></cxl-marketing-nav>
 
-      BREADCRUMBS...
+      <div class="breadcrumbs">
+        ${dataAdapter.getBreadcrumbsData().anchors.map(value => {
+          const className = value.active ? 'active' : 'inactive';
+          return html`
+            <a class="${className}" href=${value.href}>${value.content}</a>
+          `;
+        })}
+      </div>
 
       <section class="widget-odd widget-last widget-first widget-1 widget" slot="sidebar">
-        <div class="ActionsSB">
+        <div class="sidebar-published">
+          <div>Last updated:</div>
+          <div>
+            <b
+              >${// @TODO: what is wrong with this code that is not wrong in couple of lines above?
+              moment(dataAdapter.getPlaybookTimestamp()).format('DD. MMMM YYYY')}</b // eslint-disable-next-line lit/no-invalid-html
+            >
+          </div>
+        </div>
+
+        <div class="actions-sb">
           <div class="info">
             <span>Actions:</span>
           </div>
-          <div class="iconsVertical">
+          <div class="icons-vertical">
             <div class="icons">
-              RATING HERE...
+              <div class="rating">
+                <div id="with-tooltip-div" class="with-tooltip">
+                  <iron-star-rating id="iron-star-rating" icon="icons:star" />
+                  ${!userLoggedIn
+                    ? html`
+                        <paper-tooltip
+                          animationDelay="0"
+                          offset="5"
+                          position="top"
+                          htmlFor="with-tooltip-div"
+                        >
+                          Log in to vote
+                        </paper-tooltip>
+                      `
+                    : ``}
+                </div>
+              </div>
+
               <a href="#" id="share"
-                ><i><img src="" alt="Share"/></i><span>Share</span></a
+                ><i><iron-icon icon="cxl:share"></iron-icon></i><span>Share</span></a
               >
               <a href="#" id="report"
-                ><i><img src="" alt="Report"/></i><span>Report</span></a
+                ><i><iron-icon icon="cxl:report"></iron-icon></i><span>Report</span></a
+              >
+              <!-- lightbox resize image
+              <iron-icon icon="vaadin:resize-h" />
+              -->
+            </div>
+          </div>
+        </div>
+
+        <div class="author-info-sb">
+          <div class="about">
+            <span>About the author:</span>
+          </div>
+          <div class="sb-author-flex">
+            <a class="avatar">
+              <img class="avatar" src="${authorObj.avatarUrl}" alt="avatar" />
+            </a>
+            <div class="right">
+              <div class="name">
+                <a href=${authorObj.profileUrl}>${authorObj.username}</a>
+              </div>
+            </div>
+          </div>
+          <div class="bio">
+            ${// @TODO: what is wrong with this code that is not wrong in 4 lines above?
+            authorObj.bio} // eslint-disable-next-line lit/no-invalid-html
+          </div>
+        </div>
+
+        <div class="sync-with-sb">
+          <div class="sync-with-sb-content">
+            <div class="first">
+              <span>Sync with:</span>
+              <i class="question">
+                <iron-icon id="question-mark1" icon="vaadin:question-circle" />
+                <paper-tooltip
+                  animationDelay="0"
+                  offset="5"
+                  position="top"
+                  htmlFor="question-mark1"
+                >
+                  coming soon
+                </paper-tooltip>
+              </i>
+            </div>
+            <div class="icons">
+              <a href="#"
+                ><i><iron-icon icon="cxl:trello"></iron-icon></i><span>Trello</span></a
+              >
+              <a href="#"
+                ><i><iron-icon icon="cxl:asana"></iron-icon></i><span>Asana</span></a
+              >
+              <a href="#"
+                ><i><iron-icon icon="cxl:basecamp"></iron-icon></i><span>Basecamp</span></a
               >
             </div>
           </div>
@@ -69,7 +172,7 @@ export const CxlPlaybookViewerLayout = () => {
           theme="reverse"
           class="cxl-adeft-accordion-class"
         >
-          ${jsonData.data.steps.map(
+          ${dataAdapter.getAccordionData().items.map(
             el => html`
               <vaadin-accordion-panel id="step_${el.idx}" theme="reverse">
                 <div slot="summary">
