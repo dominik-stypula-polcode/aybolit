c845f657620b24509ec8b8e8aa278e571b7d0d85
Dominik Stypuła <dominik.stypula@polcode.net>
2021-02-15 13:42:24 +0100

    fix(sass-render): sass-render ignores partial SCSS files, fixed with 'attempt' to find partial parent file which imports it, and recompile it.

--- packages/sass-render/bin/sass-render.js
+++ packages/sass-render/bin/sass-render.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const fs = require('fs');
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
 const glob = require('glob');
@@ -12,14 +13,14 @@ const options = [
     alias: 's',
     type: String,
     description: 'Template file to render sass into.',
-    defaultOption: true
+    defaultOption: true,
   },
   {
     name: 'help',
     alias: 'h',
     type: Boolean,
-    description: 'Print this message.'
-  }
+    description: 'Print this message.',
+  },
 ];
 
 const { source, help } = commandLineArgs(options);
@@ -28,12 +29,12 @@ function printUsage() {
   const sections = [
     {
       header: 'sass-render',
-      content: 'Render sass into css tagged template literal'
+      content: 'Render sass into css tagged template literal',
     },
     {
       header: 'Options',
-      optionList: options
-    }
+      optionList: options,
+    },
   ];
   console.log(commandLineUsage(sections));
 }
@@ -51,9 +52,28 @@ if (!source) {
 
 glob(source, (err, files) => {
   files
-    .filter(file => !path.basename(file).startsWith('_'))
-    .forEach(file => {
-      sassRender(file).catch(error => {
+    // .filter(file => !path.basename(file).startsWith('_'))
+    .map((file) => {
+      // if not partial, process like regular file
+      const isPartial = path.basename(file).startsWith('_');
+      if (!isPartial) {
+        return file;
+      }
+
+      // if this is a partial, try to find its parent searching for `${parent_dir}.scss`
+      const mainScss = `${path.dirname(file)}.scss`;
+      if (fs.existsSync(mainScss)) {
+        return mainScss;
+      }
+
+      console.log(
+        `Warning! Partial file detected ${file} \nbut can't find file that should import it: ${mainScss}`
+      );
+      return null;
+    })
+    .filter((file) => file !== null)
+    .forEach((file) => {
+      sassRender(file).catch((error) => {
         console.error(error);
         process.exit(-1);
       });
