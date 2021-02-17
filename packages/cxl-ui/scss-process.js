c17ad427b0c17a1c018245323ac862378f244af1
Dominik Stypuła <dominik.stypula@polcode.net>
2021-02-17 16:46:37 +0100

    chore(scss-process): compressing css output files

--- packages/cxl-ui/scss-process.js
+++ packages/cxl-ui/scss-process.js
@@ -3,9 +3,15 @@ const path = require('path');
 
 const scssProcess = (mix, inputPath, outputPath) => {
   const files = fs.readdirSync(inputPath);
+  const options = {
+    sourceMap: true,
+    sassOptions: {
+      outputStyle: 'compressed'
+    }
+  };
   files.forEach(file => {
     if (path.extname(file) === '.scss' && !path.basename(file).match(/^_/g)) {
-      mix.sass(`${inputPath}/${file}`, outputPath);
+      mix.sass(`${inputPath}/${file}`, outputPath, options);
     }
   });
 };
