cab7ce69ff82a31dfe0205657d0e1ea9a3c22ff1
Dominik Stypuła <dominik.stypula@polcode.net>
2021-03-11 11:57:54 +0100

    chore(render-sidebar): only one accordion can be opened, only one category can be selected

--- packages/storybook/cxl-dashboard/partials/render-sidebar.js
+++ packages/storybook/cxl-dashboard/partials/render-sidebar.js
@@ -3,20 +3,39 @@ import sidebarData from '../data/sidebar.data.json';
 import '@conversionxl/cxl-ui/src/components/cxl-vaadin-accordion.js';
 
 // eslint-disable-next-line func-names
-const sidebarLinkClickHandler = function () {
-  this.setAttribute('clicked', true);
-  this.preventDefault();
+const sidebarCategoryClickHandler = function (event) {
+  event.preventDefault();
+
+  document
+    .querySelectorAll('#dashboard-sidebar-menu [is-category]')
+    .forEach((el) => el.removeAttribute('checked'));
+
+  if (this.parentNode.tagName.toLowerCase() === 'vaadin-accordion-panel') {
+    document
+      .querySelectorAll('#dashboard-sidebar-menu vaadin-accordion-panel')
+      .forEach((el) => el.removeAttribute('opened'));
+  }
+
+  this.setAttribute('checked', true);
+
   return false;
 };
 
 const RenderSidebarMenuItem = (menuItem) => html`
   ${menuItem.menu.length > 0
-    ? html` <vaadin-accordion-panel theme="cxl-hub-sidebar">
-        <div slot="summary">${menuItem.title}</div>
-        <div class="cxl-hub-items">
+    ? html` <vaadin-accordion-panel theme="cxl-dashboard-sidebar">
+        <div is-category slot="summary" @click="${sidebarCategoryClickHandler}">
+          ${menuItem.title}
+        </div>
+        <div id="cxl-dashboard-items" class="cxl-hub-items">
           ${menuItem.menu.map(
             (subMenu) => html`
-              <a cxl-sidebar-link href="${subMenu.url}" @click="${sidebarLinkClickHandler}">
+              <a
+                is-category
+                cxl-sidebar-link
+                href="${subMenu.url}"
+                @click="${sidebarCategoryClickHandler}"
+              >
                 <span cxl-sidebar-title>${subMenu.title}</span>
                 <span cxl-sidebar-playbooks-count>${subMenu.playbooks_count}</span>
               </a>
@@ -24,7 +43,11 @@ const RenderSidebarMenuItem = (menuItem) => html`
           )}
         </div>
       </vaadin-accordion-panel>`
-    : html` <a one-level href="${menuItem.url}" theme="cxl-hub-sidebar">${menuItem.title}</a> `}
+    : html`
+        <a is-category one-level href="${menuItem.url}" @click="${sidebarCategoryClickHandler}"
+          >${menuItem.title}</a
+        >
+      `}
 `;
 
 const RenderSidebar = () =>
