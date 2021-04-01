88052bdaaa2dcc4559603af79a56eb6a1dcd6812
Dominik Stypuła <dominik.stypula@polcode.net>
2021-04-01 17:18:00 +0200

    refactor(cxl-search): css moved to scss files, redundant code removed

--- packages/storybook/cxl-ui/cxl-search/layout=1c-w.stories.js
+++ packages/storybook/cxl-ui/cxl-search/layout=1c-w.stories.js
@@ -1,6 +1,9 @@
 import { html } from 'lit-html';
 import '@conversionxl/cxl-ui/src/components/cxl-app-layout.js';
 import '@conversionxl/cxl-ui/src/components/cxl-marketing-nav.js';
+import cxlSearchStyles from '@conversionxl/cxl-ui/src/styles/cxl-search/cxl-search-css.js';
+import cxlSearchFiltersStyles from '@conversionxl/cxl-ui/src/styles/cxl-search/cxl-search-filters-css.js';
+import { registerGlobalStyles } from "@conversionxl/cxl-lumo-styles/src/utils";
 import { CXLMarketingNav } from '../cxl-marketing-nav.stories';
 import { CXLFooterNav } from '../footer-nav.stories';
 import { displaySearchFilters } from './displaySearchFilters';
@@ -11,88 +14,16 @@ export default {
   title: 'CXL Search',
 };
 
+// CXL Search global
+registerGlobalStyles(cxlSearchStyles, {
+  moduleId: 'cxl-search-global',
+});
+// CXL Search filter dropdowns
+registerGlobalStyles(cxlSearchFiltersStyles, {
+  moduleId: 'cxl-search-global',
+});
+
 export const CXLSearch = () => html`
-  <style>
-    .card {
-      padding: var(--lumo-space-m);
-      margin-top: var(--lumo-space-m);
-      margin-bottom: var(--lumo-space-m);
-      overflow: hidden;
-      background-color: var(--lumo-tint);
-      border: 1px solid var(--lumo-shade-10pct);
-      border-radius: var(--lumo-border-radius-s);
-      box-shadow: var(--lumo-box-shadow-xs);
-      break-inside: avoid;
-      transform: translateZ(0);
-    }
-    .filters {
-      display: flex;
-    }
-    #sort-combo {
-      margin-left: auto;
-    }
-    #topic-combo {
-      margin-left: var(--lumo-space-l);
-    }
-    .result-entry {
-      margin-top: var(--lumo-space-xl);
-    }
-    .result-entry h4 {
-      margin-bottom: var(--lumo-space-xs);
-    }
-    .results-info {
-        color: var(--lumo-tertiary-text-color);
-    }
-    .result-info {
-      font-size: var(--lumo-font-size-s);
-      margin-bottom: var(--lumo-space-xs);
-    }
-    .result-info span {
-      color: var(--lumo-primary-text-color);
-    }
-
-    .results-pagination {
-      display:flex;
-      justify-content: center;
-      margin-top: var(--lumo-space-xl);
-    }
-    .results-pagination div {
-      padding: var(--lumo-space-s);
-    }
-    .results-pagination a {
-      color: var(--lumo-primary-color);
-      font-weight: 600;
-    }
-
-    .pagination-arrow-right div {
-      padding:0;
-    }
-    .pagination-arrow-right a {
-      font-size: var(--lumo-icon-size-xs);
-      font-weight: normal;
-      font-family: 'lumo-icons';
-      border-radius: 50%;
-      align-self: center;
-      display: block;
-      width: 1em;
-      height: 1em;
-      margin-left: calc(var(--lumo-space-xs) * -1);
-      margin-right: var(--lumo-space-xs);
-      line-height: 1;
-      padding: var(--lumo-space-xs);
-      color: var(--lumo-primary-color);
-      background-color: var(--lumo-primary-color-10pct);
-    }
-    .pagination-arrow-right a:hover {
-      text-decoration: none;
-    }
-    .pagination-arrow-right a::after{
-      content: var(--lumo-icons-angle-right);
-
-    }
-
-
-  </style>
   <cxl-app-layout id="container" layout="1c-w">
     ${CXLMarketingNav()}
     <article class="entry">
