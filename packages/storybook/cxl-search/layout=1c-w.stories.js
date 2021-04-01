c4f3074cab0400407ec48214ee3ffb231169fdd5
Dominik Stypuła <dominik.stypula@polcode.net>
2021-04-01 14:36:22 +0200

    feat(display-search-filter): search results with pagination

--- packages/storybook/cxl-search/layout=1c-w.stories.js
+++ packages/storybook/cxl-search/layout=1c-w.stories.js
@@ -4,6 +4,8 @@ import '@conversionxl/cxl-ui/src/components/cxl-marketing-nav.js';
 import { CXLMarketingNav } from '../cxl-ui/cxl-marketing-nav.stories';
 import { CXLFooterNav } from '../cxl-ui/footer-nav.stories';
 import { displaySearchFilters } from './displaySearchFilters';
+import searchResults from './data/search-results.data.json';
+import searchPaginationData from './data/search-pagination.data.json';
 
 export default {
   title: 'CXL Search',
@@ -32,10 +34,67 @@ export const CXLSearch = () => html`
     #topic-combo {
       margin-left: var(--lumo-space-l);
     }
+    .result-entry {
+      margin-top: var(--lumo-space-xl);
+    }
+    .result-entry h4 {
+      margin-bottom: var(--lumo-space-xs);
+    }
+    .results-info {
+        color: var(--lumo-tertiary-text-color);
+    }
+    .result-info {
+      font-size: var(--lumo-font-size-s);
+      margin-bottom: var(--lumo-space-xs);
+    }
+    .result-info span {
+      color: var(--lumo-primary-text-color);
+    }
+
+    .results-pagination {
+      display:flex;
+      justify-content: center;
+      margin-top: var(--lumo-space-xl);
+    }
+    .results-pagination div {
+      padding: var(--lumo-space-s);
+    }
+    .results-pagination a {
+      color: var(--lumo-primary-color);
+      font-weight: 600;
+    }
+
+    .pagination-arrow-right div {
+      padding:0;
+    }
+    .pagination-arrow-right a {
+      font-size: var(--lumo-icon-size-xs);
+      font-weight: normal;
+      font-family: 'lumo-icons';
+      border-radius: 50%;
+      align-self: center;
+      display: block;
+      width: 1em;
+      height: 1em;
+      margin-left: calc(var(--lumo-space-xs) * -1);
+      margin-right: var(--lumo-space-xs);
+      line-height: 1;
+      padding: var(--lumo-space-xs);
+      color: var(--lumo-primary-color);
+      background-color: var(--lumo-primary-color-10pct);
+    }
+    .pagination-arrow-right a:hover {
+      text-decoration: none;
+    }
+    .pagination-arrow-right a::after{
+      content: var(--lumo-icons-angle-right);
+
+    }
+
+
   </style>
   <cxl-app-layout id="container" layout="1c-w">
     ${CXLMarketingNav()}
-
     <article class="entry">
       <header class="entry-header">
         <label>Page</label>
@@ -44,8 +103,31 @@ export const CXLSearch = () => html`
       <div class="entry-content">
         <div class="card filters">${displaySearchFilters()}</div>
       </div>
+      <div class="results-info">
+        Showing results 1 - 15 of 277
+      </div>
+      <div class="results-entries">
+        ${searchResults.map((el) => html`
+          <div class="result-entry">
+            <h4>${el.title}</h4>
+            <div class="result-info"><span>${el.category}</span>,  Author: ${el.author}, ${el.date}</div>
+            <div class="result-description">${el.description}</div>
+          </div>`
+        )}
+      </div>
+      <div class="results-pagination">
+        ${searchPaginationData.pages.map( (el) =>
+          html`
+            ${el.url ? html`<div><a href="${el.url}">${el.page}</a></div>` : html`<div>${el.page}</div>`}
+          `)
+         }
+          <div>...</div>
+          <div><a href="${searchPaginationData.lastPage.url}">${searchPaginationData.lastPage.page}</a></div>
+          <div class="pagination-arrow-right">
+            <a href="${searchPaginationData.nextPage.url}"></a>
+          </div>
+      </div>
     </article>
-
     ${CXLFooterNav()}
   </cxl-app-layout>
 `;
