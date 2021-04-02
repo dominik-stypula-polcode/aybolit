31c8c6b8756fdcc2098d71977a130a788df9b0e7
Dominik Stypuła <dominik.stypula@polcode.net>
2021-04-02 16:48:19 +0200

    feat(cxl-playbook-accordion): disable frontend hyperlinks in steps and step explanations

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
@@ -2,6 +2,11 @@ import { html } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import playbookStepData from './cxl-playbook-accordion.data.json';
 
+const render = (htmlStr) => {
+    const htmlWithoutAnchors = htmlStr.replace(/<a.*?>(.*?)<\/a>/gi, "<span class=\"replaced-for-anchor\">$1</span>");
+    return unsafeHTML(htmlWithoutAnchors);
+}
+
 export const CXLPlaybookAccordion = ({ FeedbackButtonLabel, PlaybookId }) => html`
   <cxl-playbook-accordion id="cxl-playbook-accordion-${PlaybookId}" class="plural" opened="0">
     ${playbookStepData.map(
@@ -10,10 +15,10 @@ export const CXLPlaybookAccordion = ({ FeedbackButtonLabel, PlaybookId }) => htm
           <header class="entry-header" slot="summary">
             <vaadin-checkbox value="${el.id}"></vaadin-checkbox>
             <h3 class="entry-title no-anchor" itemprop="headline">
-              ${el.id}. ${unsafeHTML(el.title.rendered)}
+              ${el.id}. ${render(el.title.rendered)}
             </h3>
           </header>
-          <div class="entry-summary" itemprop="description">${unsafeHTML(el.content.rendered)}</div>
+          <div class="entry-summary" itemprop="description">${render(el.content.rendered)}</div>
           <div class="entry-footer">
             <vaadin-button
               onclick="alert('Step ID: ' + this.closest('vaadin-accordion-panel').dataset.stepId)"
