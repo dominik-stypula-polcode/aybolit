7537ea5cf12718d3ac9c30e66ba5033a49a16e8b
Dominik Stypuła <dominik.stypula@polcode.net>
2021-07-05 15:11:44 +0200

    feat: fastcomments.com introduced in the accordion step

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
@@ -3,9 +3,11 @@ import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import playbookStepData from './cxl-playbook-accordion.data.json';
 
 export const CXLPlaybookAccordion = ({ PlaybookId }) => html`
+  <script src="https://cdn.fastcomments.com/js/embed-v2.min.js"></script>
+
   <cxl-playbook-accordion id="cxl-playbook-accordion-${PlaybookId}" class="plural" opened="0">
     ${playbookStepData.map(
-      (el, index) => html`
+      (el) => html`
         <vaadin-accordion-panel theme="cxl-playbook-accordion reverse" data-step-id="${el.id}">
           <header class="entry-header" slot="summary">
             <vaadin-checkbox value="${el.id}"></vaadin-checkbox>
@@ -15,17 +17,12 @@ export const CXLPlaybookAccordion = ({ PlaybookId }) => html`
           </header>
           <div class="entry-summary" itemprop="description">${unsafeHTML(el.content.rendered)}</div>
           <div class="entry-footer">
-            ${index === 0
-              ? html`
-                  <script
-                    defer
-                    src="https://cdn.commento.io/js/commento.js"
-                    data-id-root="commento-${el.id}"
-                    data-auto-init="true"
-                  ></script>
-                  <div id="commento-${el.id}"></div>
-                `
-              : ``}
+            <div id="fastcomments-widget-${el.id}"></div>
+            <script>
+              window.FastCommentsUI(document.getElementById('fastcomments-widget-${el.id}'), {
+                tenantId: 'demo',
+              });
+            </script>
           </div>
         </vaadin-accordion-panel>
       `
